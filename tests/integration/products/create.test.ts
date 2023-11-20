b5bc538652b4dab5bd463b91a148e7c709f7e9b6
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-11-20 16:39:31 -0300

    start of product creation integration tests

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,29 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import productMock from '../../mocks/product.mock';
+import app from '../../../src/app';
+import ProductModel from '../../../src/database/models/product.model';
 
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
+  it.only('Should register a product successfully', async function  () {
+    const httpRequestBody = productMock.productValid
 
+    const mockCreatedReturn = ProductModel.build(productMock.createProductFromDB);
+
+    sinon.stub(ProductModel, 'create').resolves(mockCreatedReturn);
+
+   
+    const httpResponse = await chai.request(app).post('/products').send(httpRequestBody);
+
+    expect(httpResponse.status).to.equal(201);
+    expect(httpResponse.body).to.be.deep.equal({
+      id: 6,
+      name: "Martelo de Thor",
+      price: "30 peças de ouro"
+    });
+  });
 });
---
+++ tests/mocks/product.mock.ts
@@ -0,0 +1,17 @@
+const productValid = {
+    name: "Martelo de Thor",
+    price: "30 peças de ouro",
+    orderId: 4
+}
+
+const createProductFromDB = {
+    id: 6,
+    name: "Martelo de Thor",
+    price: "30 peças de ouro",
+    orderId: 4,
+}
+
+export default {
+    productValid,
+    createProductFromDB
+}
