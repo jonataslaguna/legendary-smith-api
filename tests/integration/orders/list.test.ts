8a43057d273aadf8896044caadd78b7e2e7b4ce6
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-11-21 12:17:34 -0300

    integration test list orders

--- tests/integration/orders/list.test.ts
+++ tests/integration/orders/list.test.ts
@@ -1,10 +1,22 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+import ordersMock from '../../mocks/orders.mock';
+import OrderModel from '../../../src/database/models/order.model';
 
 chai.use(chaiHttp);
 
 describe('GET /orders', function () { 
   beforeEach(function () { sinon.restore(); });
+  it('Should list all orders', async function  () {
+    
+    const mockFindAllReturn =  OrderModel.build(ordersMock.allOrdersFromDB);
 
+    sinon.stub(OrderModel, 'findAll').resolves([mockFindAllReturn]);
+   
+    const httpResponse = await chai.request(app).get('/orders');
+
+    expect(httpResponse.status).to.equal(200);
+  });
 });
---
+++ tests/mocks/orders.mock.ts
@@ -0,0 +1,13 @@
+const allOrdersFromDB = {
+    id: 1,
+    userId: 1,
+    productIds: [
+        {id: 1},
+        {id: 2},
+    ]
+};
+
+
+export default {
+    allOrdersFromDB,
+}
